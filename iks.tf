e4eb6212e79296ac4b6bef77831d6b57ed8014be
jcolcombe <jcolcombe@outlook.com>
2021-06-22 13:44:29 +0100

    Update iks.tf

--- iks.tf
+++ iks.tf
@@ -1,6 +1,6 @@
 module "iks" {
   source              = "terraform-cisco-modules/iks/intersight"
-  version             = "0.9.20"
+  version             = "1.0.0"
 
   # Infra Config Policy Information
   cluster_name        = var.cluster_name
