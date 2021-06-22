3500d1572616a5e95a462bfcf97b0da9d1d54564
jcolcombe <jcolcombe@outlook.com>
2021-06-22 10:39:39 +0100

    Update iks.tf

--- iks.tf
+++ iks.tf
@@ -53,5 +53,28 @@ module "iks" {
   # Organization
   organization        = var.organization
   
+}
+
   
+module "addons" {
+
+  source = "terraform-cisco-modules/iks/intersight//modules/addon_policy"
+  addons = [{
+    addon_policy_name = "dashboard"
+    addon             = "kubernetes-dashboard"
+    description       = "K8s Dashboard Policy"
+    upgrade_strategy  = "AlwaysReinstall"
+    install_strategy  = "InstallOnly"
+    },
+    {
+      addon_policy_name = "monitor"
+      addon             = "ccp-monitor"
+      description       = "Grafana Policy"
+      upgrade_strategy  = "AlwaysReinstall"
+      install_strategy  = "InstallOnly"
+    }
+  ]
+
+  org_name = var.organization
+  tags     = var.tags
 }
