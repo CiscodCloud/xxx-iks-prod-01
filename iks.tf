204abd69d527f2b956b3899c4fb6d6c272ba040c
jcolcombe <jcolcombe@outlook.com>
2021-06-24 20:31:31 +0100

    Update iks.tf

--- iks.tf
+++ iks.tf
@@ -57,4 +57,7 @@ module "iks" {
   ]
 }
 
-  
+  #Wait for cluster to come up and then outpt the kubeconfig, if successful
+  output "kube_config" {
+  	value = intersight_kubernetes_cluster_profile.kubeprofaction.kube_config[0].kube_config
+  }
