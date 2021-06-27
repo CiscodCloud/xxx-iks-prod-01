ab94e11afca2bce3882c388d8b97ca6d5153755b
jcolcombe <jcolcombe@outlook.com>
2021-06-27 13:19:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-required_providers {
+
+terraform {
+  required_version = ">=0.14.5"
+
+  required_providers {
     intersight = {
       source  = "CiscoDevNet/terraform-provider-intersight"
       version = "=1.0.10"
     }
   }
+}
 
 provider "intersight" {
   apikey    = var.apikey
