37f7be2427db6c661c7ebaffa3846db74678e701
jcolcombe <jcolcombe@outlook.com>
2021-06-27 13:41:33 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "cluster_moid" {
-  #value = module.terraform-intersight-iks.cluster_moid
-  value = module.iks.cluster_moid
+  value = module.terraform-intersight-iks.cluster_moid
+  #value = module.iks.cluster_moid
 }
